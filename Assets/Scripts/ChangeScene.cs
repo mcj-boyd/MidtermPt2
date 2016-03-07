7167f29a40d8dcb92127f6812604c3e05e46c58b
Matthew Boyd <boyd0077@algonquinlive.com>
2016-03-07 11:32:25 -0500

    Commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+
+    public void ChangeSceneWithName(string sceneName)
+    {
+        // Load the Scene specified by the button click.
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,6 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour {
   public GameObject ball;
+    public AudioClip goalSound;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ball"))
+        {
+            AudioSource.PlayClipAtPoint(goalSound, transform.position);
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
 }
